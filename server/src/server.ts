c63cd2332833cdb92593ec8f87165d2a277d4d15
NicolasESchneider <nicoefschneider@gmail.com>
2021-04-04 20:01:55 -0400

    adding Websocket broadcast logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,18 +12,58 @@ const server = http.createServer(app)
 
 const wss = new WebSocket.Server({ server })
 
-wss.on('connection', (ws: WebSocket) => {
-  // connection is up.
-  ws.on('message', (message: string) => {
-    // log the message, pass it back to the client
+interface ExtWebSocket extends WebSocket {
+  isAlive?: boolean;
+}
 
-    console.log('received: %s', message)
-    ws.send('Hello, you sent -> ' + message)
-  })
+
+wss.on('connection', (ws: ExtWebSocket) => {
+  // connection is up.
+
+  ws.isAlive = true;
+
+  ws.on('pong', () => {
+    console.log('pinged')
+    ws.isAlive = true;
+  });
+
+  ws.on('message', (message: string) => {
+
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+
+    const broadcastRegex = /^broadcast\:/;
+
+    if (broadcastRegex.test(message)) {
+      message = message.replace(broadcastRegex, '');
+
+      //send back the message to the other clients
+      wss.clients
+        .forEach(client => {
+          if (client != ws) {
+            client.send(`Hello, broadcast message -> ${message}`);
+          }    
+        });
+        
+    } else {
+      ws.send(`Hello, you sent -> ${message}`);
+    }
+});
 
   //send immediatly a feedback to the incoming connection    
   ws.send('Hi there, I am a WebSocket server');
 })
+
+setInterval(() => {
+  wss.clients.forEach((ws: ExtWebSocket) => {
+    
+    if (!ws.isAlive) return ws.terminate();
+    
+    ws.isAlive = false;
+    ws.ping(null, false);
+
+  });
+}, 10000);
 
 server.listen(process.env.PORT || 8999, () => {
   console.log(`Server started on port ${process.env.PORT || 8999} :)`);
